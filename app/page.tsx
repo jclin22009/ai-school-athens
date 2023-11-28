f63f4729f728c8466a6a87054966e8ecb734561e
Jason Lin <jclin2.2009@gmail.com>
2023-11-28 13:33:06 -0800

    working coords

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,29 @@
+"use client"
 import Image from 'next/image'
-import styles from './page.module.css'
+import ImageMapper from 'react-img-mapper'
 
 export default function Home() {
+
+  const athensMap = {
+    name: "my-map",
+    areas: [
+      { id: "Plato", shape: "circle", coords: [1817, 1316, 70], href: "https://www.google.com" },
+      { id: "Aristotle", shape: "circle", coords: [1963, 1311, 70], href: "https://www.google.com" },
+      { id: "Anaximander", shape: "circle", coords: [765, 1926, 70], href: "https://www.google.com" },
+      { id: "Socrates", shape: "circle", coords: [1324, 1307, 70], href: "https://www.google.com" },
+      { id: "Diogenes", shape: "circle", coords: [2146, 1738, 70], href: "https://www.google.com" },
+      { id: "Heraclitus", shape: "circle", coords: [1648, 1870, 70], href: "https://www.google.com" },
+      { id: "Epicurus", shape: "circle", coords: [573, 1611, 70], href: "https://www.google.com" },
+      { id: "Euclid", shape: "circle", coords: [2997, 1920, 70], href: "https://www.google.com" },
+      { id: "Ptolemy", shape: "circle", coords: [3321, 1620, 70], href: "https://www.google.com" },
+      { id: "Pythagoras", shape: "circle", coords: [948, 1846, 70], href: "https://www.google.com" },
+      { id: "Zoroaster", shape: "circle", coords: [3245, 1593, 70], href: "https://www.google.com" },
+    ]
+  };
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div style={{ width: '100%', height: '100%' }}>
+      <p>Hover over a philosopher's face to talk to them!</p>
+      <ImageMapper src={'/athens_school.jpg'} map={athensMap} responsive={true} natural={true} parentWidth={700} />
+    </div>
   )
 }
