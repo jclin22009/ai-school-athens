eed747e11792f3d7a63ff32edcb0ab8c9759effe
Jason Lin <jclin2.2009@gmail.com>
2023-11-29 17:29:04 -0800

    add sys prompt to chat, scroll window

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -1,38 +1,45 @@
-'use client';
+"use client";
 
-import { Box, Button, HStack, Input } from '@chakra-ui/react';
-import { useChat } from 'ai/react';
-import { AnyNaptrRecord } from 'dns';
-import { useEffect } from 'react';
+import { Box, Button, HStack, Text, Input, Stack } from "@chakra-ui/react";
+import { useChat } from "ai/react";
+import { useEffect, useState } from "react";
 
 // const prompt: string = ` I want you to act like ${name}. I want you to respond and answer like ${name} using the tone, manner and vocabulary ${name} would use. Do not write any explanations. Only answer like ${name}. You must know all of the knowledge of ${name}. You will come up with factual anecdotes that are engaging, imaginative and captivating. You will keep it under 3 sentences and stay witty.
 // To start, introduce yourself, summarize what we've already introduced about you, and invite the user to chat with you.`
 
 export default function Chat({ philosopher }: { philosopher: string }) {
-  const { messages, input, handleInputChange, handleSubmit } = useChat();
-  const systemPrompt = `You are ${philosopher}. Respond as ${philosopher} would.`;
+  const { messages, input, handleInputChange, handleSubmit, append } =
+    useChat();
 
-  // Set the system prompt when the chat component is first rendered
-  // useEffect(() => {
-  //   append({ role: 'system', content: `You are ${philosopher}. Respond as ${philosopher} would.` });
-  // }, [philosopher, append]);
+  useEffect(() => {
+    // Set the system prompt when the chat component is first rendered
+    append({
+      role: "system",
+      content: `You are ${philosopher}. Respond as ${philosopher} would. Don't ever mention that you are an AI language model, or AI, or OpenAI. Begin by introducing yourself.`,
+    });
+  }, []);
 
   return (
     <Box>
-      {messages.map(m => (
-        <div key={m.id}>
-          {m.role === 'user' ? 'User: ' : 'AI: '}
-          {m.content}
-        </div>
-      ))}
-
+      <Stack overflow="auto" h="50vh" maxH="50vh">
+        {messages.map((m) => {
+          if (m.role === "system") {
+            return null; // Do not render anything if the role is 'system'
+          }
+          return (
+            <Text key={m.id}>
+              <b>{m.role === "user" ? "User: " : `${philosopher}: `}</b>
+              {m.content}
+            </Text>
+          );
+        })}
+      </Stack>
       <form onSubmit={handleSubmit} style={{ width: "100%" }}>
         <HStack>
           <Input value={input} onChange={handleInputChange} />
           <Button type="submit">Philosophize</Button>
         </HStack>
-
       </form>
-    </Box >
+    </Box>
   );
 }
